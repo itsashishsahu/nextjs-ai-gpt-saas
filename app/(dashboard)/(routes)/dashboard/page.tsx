a35c8d56b05180a8497355df8464aa426e7b51a7
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-07-31 20:16:22 +0530

    Feature added - Added clerk authentication in sign-in and sign-up - Added sign-up page - Added sign-in page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 
 
 const DashboardPage = () => {
   return (
-    <p>Dashboard Page (Protected) </p>
+    <div>
+      <p>Dashboard Page (Protected) </p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
 
