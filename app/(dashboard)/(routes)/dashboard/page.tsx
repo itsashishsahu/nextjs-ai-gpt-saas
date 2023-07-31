31c355a57b18900dcd242bce01b9a35da3be91d8
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-07-31 22:38:55 +0530

    Feature added - Added navbar component - Added logo - Added sidebar component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-import DashboardLayout from "../../layout";
-
-
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard Page (Protected) </p>
-      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({
     children
 } : {
@@ -5,15 +8,13 @@ const DashboardLayout = ({
 }) => {
   return (
     <div className="h-full relative">
-        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-            <div>
-                Hello Sidebar
-            </div>
+        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+        <Sidebar />
         </div>
-        <main className="md:pl-72">
-            <NavBar />
+        <main className="md:pl-72 pb-10">
+            <Navbar />
            { children }
-        </main>
+        </main> 
     </div>
   )
 }
