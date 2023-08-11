801d5c3da9b0cf274793efa00e8872f0ec7bab17
Ashish Sahu <itsashishsahu.inc@gmail.com>
2023-08-12 02:00:58 +0530

    * Major update - 1. Integrated OpenAi API for conversation. 2. Added features in conversation page. 3. Added avatar for user and bot using shadcn avatar.

---
+++ components/loader.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export const Loader = () => {
+  return (
+    <div className="h-full flex flex-col gap-y-4 items-center justify-center">
+        <div className="w-10 h-10 realtive animate-spin">
+            <Image 
+            alt="logo"
+            fill
+            src="/logo.png"
+            />
+        </div>
+        <p className="text-sm text-muted-foreground">
+            Ashish is thinking...
+        </p>
+    </div>
+  );
+}
+
